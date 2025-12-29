a462bb671845832fd7eaa5ecd4cb92f5e9346d5e
Sendy Kurniawan <@sendykurniawan2006>
2025-12-29 18:14:41 +0800

    Your commit message here

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\foundation\Bus\DispatchesJobs;
-use Illuminate\foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;;
 
 class Controller extends BaseController
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -7,28 +7,25 @@
   <title>Menu Kami</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/stylew.css">
-   <!-- font -->
-   <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
-      rel="stylesheet"
-    />
-
-    <!-- feather icons -->
-    <script src="https://unpkg.com/feather-icons"></script>
+  <!-- font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+    rel="stylesheet"
+  />
+  <!-- feather icons -->
+  <script src="https://unpkg.com/feather-icons"></script>
 </head>
 <body>
 <nav class="navbar">
     <a href="/" class="back"><-Back</a>
-
-
-      <div class="navbar-extra">
-        <a href="#" id="search"><i data-feather="search"></i></a>
-        <a href="#" id="shopping"><i data-feather="shopping-cart"></i></a>
-        <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
-      </div>
-    </nav>
+    <div class="navbar-extra">
+      <a href="#" id="search"><i data-feather="search"></i></a>
+      <a href="#" id="shopping"><i data-feather="shopping-cart"></i></a>
+      <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+    </div>
+  </nav>
 
   <section id="menu" class="menu">
     <h2><span>Menu</span> Kami</h2>
@@ -36,20 +33,45 @@
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magnam
       officia eius explicabo, pariatur veritatis?
     </h4>
-
     
     <div class="row">
       @foreach ($data as $row)
-      <div class="box">
-      <div class="menu-card">
-        <img src="{{ asset('fotomenu/'.$row->foto) }}" alt="Espreso" class="menu-card-img" />
-        <h3 class="menu-card-tittle">{{$row->nama}}</h3>
-        <p class="menu-card-price">{{ frupiah($row->harga)}}</p>
-        <a href="#" class="cta">Beli</a>
-      </div>
-    </div>
-    @endforeach 
+        <?php
+          $imagePath = 'fotomenu/'.$row->foto;
+          $extension = strtolower(pathinfo($row->foto, PATHINFO_EXTENSION));
+          $isJpg = in_array($extension, ['jpg', 'jpeg', 'jfif']);
+          $isPng = in_array($extension, ['png']);
+          
+          $boxStyle = '';
+          
+          if ($isJpg) {
+            $boxStyle = "style=\"background-image: url('" . asset($imagePath) . "');
+            
+            border-bottom: 0px;
+            background-position: center center;\"";
+          }
+        ?>
+
+        <div class="box" <?php echo $boxStyle; ?>>
+          <div class="menu-card"<?php if ($isJpg): echo "style=\"background: linear-gradient(to top, rgba(0,0,0,0.9), transparent); border-radius: 0 0 20px 20px;\""; endif; ?>>
+            <div>
+              <?php if ($isPng): ?>
+                <img src="{{ asset('fotomenu/'.$row->foto) }}" alt="{{$row->nama}}" class="menu-card-img" />
+              <?php endif; ?>
+              <h3 <?php if ($isJpg): echo "class=\"has-jpg-bg-title\""; else :
+                echo "class=\"menu-card-title\""; endif; ?>>{{$row->nama}}</h3>
+              <p <?php if ($isJpg): echo "class=\"has-jpg-bg-price\""; else :
+                echo "class=\"menu-card-price\""; endif; ?>>{{ frupiah($row->harga)}}</p>
+              <a href="#" class="cta">Beli</a>
+            </div>
+          </div>
+        </div>
+      @endforeach 
     </div>
   </section>
+
+  <script>
+    feather.replace();
+  </script>
 </body>
 </html>
